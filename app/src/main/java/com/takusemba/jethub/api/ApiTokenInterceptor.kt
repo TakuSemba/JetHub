45c7e5a095dd732c9e4efa77ba9fcc7c1779ace1
takusemba <taku1008ele@gmail.com>
2019-01-01 14:04:06 +0900

    add comment

--- app/src/main/java/com/takusemba/jethub/api/ApiTokenInterceptor.kt
+++ app/src/main/java/com/takusemba/jethub/api/ApiTokenInterceptor.kt
@@ -17,6 +17,7 @@ class ApiTokenInterceptor : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     val token = BuildConfig.API_KEY
     val builder = chain.request().newBuilder()
+    // API_KEY will be `null` if nothing is set in local.properties
     if (token.isNotBlank() && token != "null") {
       builder.addHeader(AUTHORIZATION_HEADER, "token ${BuildConfig.API_KEY}")
     }
