a7709df78dc595299f81072d0a337ddf66fb3f4d
takusemba <takusemba.ele@gmail.com>
2020-05-31 15:52:56 +0900

    remove unwanted impots

--- app/src/main/java/com/takusemba/jethub/ui/fragment/PinFragment.kt
+++ app/src/main/java/com/takusemba/jethub/ui/fragment/PinFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
