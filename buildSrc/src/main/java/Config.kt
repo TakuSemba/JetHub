0e2981bc970dcc118099648fc2d2c3efd2303a3b
takusemba <takusemba.ele@gmail.com>
2020-05-31 16:09:06 +0900

    update target sdk version 29

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -3,7 +3,7 @@ object Config {
   val versionName = "1.0.0"
   val versionCode = 1
   val packageName = "com.takusemba.jethub"
-  val compileSdkVersion = 28
-  val targetSdkVersion = 28
+  val compileSdkVersion = 29
+  val targetSdkVersion = 29
   val minSdkVersion = 21
 }
