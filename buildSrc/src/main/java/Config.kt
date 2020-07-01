50042c5dbe7038912da76dbf1497b1f68d982052
takusemba <takusemba.ele@gmail.com>
2020-07-01 14:49:22 +0900

    use constants.gralde for config properties

--- buildSrc/src/main/java/Config.kt
+++
@@ -1,9 +0,0 @@
-object Config {
-  // App
-  val versionName = "1.0.0"
-  val versionCode = 1
-  val packageName = "com.takusemba.jethub"
-  val compileSdkVersion = 29
-  val targetSdkVersion = 29
-  val minSdkVersion = 21
-}
