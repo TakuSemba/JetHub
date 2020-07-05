cef96529f44cce17a34d0c56a2c0c46033579f04
takusemba <takusemba.ele@gmail.com>
2020-07-05 12:24:37 +0900

    navigation to DeveloperFragment

--- dynamic/repo/src/main/java/com/takusemba/jethub/repo/RepoFragment.kt
+++ dynamic/repo/src/main/java/com/takusemba/jethub/repo/RepoFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.Recomposer
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
@@ -19,9 +20,28 @@ import androidx.ui.layout.padding
 import androidx.ui.material.Button
 import androidx.ui.text.style.TextAlign
 import androidx.ui.unit.dp
+import com.takusemba.jethub.base.viewmodel.NavigationViewModel
 import com.takusemba.jethub.compose.JethubTheme
+import com.takusemba.jethub.di.RepoModuleDependencies
+import dagger.hilt.android.EntryPointAccessors
 
 class RepoFragment : Fragment(R.layout.fragment_repo) {
+
+  private val navigationViewModel: NavigationViewModel by activityViewModels()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    DaggerRepoComponent.builder()
+      .context(requireContext())
+      .appDependencies(
+        EntryPointAccessors.fromApplication(
+          requireContext().applicationContext,
+          RepoModuleDependencies::class.java
+        )
+      )
+      .build()
+      .inject(this)
+  }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
@@ -47,7 +67,7 @@ class RepoFragment : Fragment(R.layout.fragment_repo) {
               modifier = Modifier.padding(16.dp)
             )
             Button(
-              onClick = { } // TODO navigate to DeveloperFragment
+              onClick = { navigationViewModel.openDeveloper() } // TODO navigate to DeveloperFragment
             ) {
               Text(
                 text = "Go to Developer Fragment",
