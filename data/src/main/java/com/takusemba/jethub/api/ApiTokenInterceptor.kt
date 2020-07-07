37cd8f504e167d72c89b4c0bc83ff343a88cd5cf
takusemba <takusemba.ele@gmail.com>
2020-07-07 22:40:50 +0900

    fix github api token

--- data/src/main/java/com/takusemba/jethub/api/ApiTokenInterceptor.kt
+++ data/src/main/java/com/takusemba/jethub/api/ApiTokenInterceptor.kt
@@ -17,8 +17,8 @@ class ApiTokenInterceptor : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
     val token = BuildConfig.GITHUB_API_TOKEN
     val builder = chain.request().newBuilder()
-    // GITHUB_API_TOKEN will be `null` if nothing is set in local.properties
-    if (token.isNotBlank() && token != "null") {
+    // GITHUB_API_TOKEN will be empty if nothing is set in local.properties
+    if (token.isNotEmpty()) {
       builder.addHeader(AUTHORIZATION_HEADER, "token $token")
     }
     return chain.proceed(builder.build())
